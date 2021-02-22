e73752ec1c9eefd26bfdf3937d6884f33094db90
Marcelo Zambrana Villarroel <marcech.am@gmail.com>
2021-02-22 11:12:09 -0500

    backend generic configuration

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 terraform {
-  backend "remote" {
-    organization = "zambrana"
-
+  # It is recommended to use remote state instead of local
+  # You can update these values in order to configure your remote state.
+  /*  backend "remote" {
+    organization = "{{ORGANIZATION_NAME}}"
     workspaces {
-      name = "DoT-DemoWindowsVM"
+      name = "{{WORKSPACE_NAME}}"
     }
   }
+*/
   required_version = "= 0.14.7"
   required_providers {
     azurerm = "=2.48.0"
